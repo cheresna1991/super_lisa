21b3a23d45354de4923fb7a982641c5149f75ec1
Cheresna <yaroslavcheg98@gmail.com>
2025-02-15 19:54:43 +0800

    добавил текстовые задачи

--- js/text.js
+++ js/text.js
@@ -1,44 +1,44 @@
 let a = document.getElementById('aaa');
 a.addEventListener('click', () => {
     var ia = Number(document.querySelector('#ia>input').value);
-    if (ia === 60) {
+    if (ia === 0.6) {
         var g = 'Правильно!';
         document.getElementById("aa").innerHTML = g;
     } else {
-        var g = 'Неправильно! Ответ: 98';
+        var g = 'Неправильно! Ответ: 0.6';
         document.getElementById("aa").innerHTML = g;
     }
 });
 let b = document.getElementById('aab');
 b.addEventListener('click', () => {
     var ib = Number(document.querySelector('#ib>input').value);
-    if (ib === 90)  {
+    if (ib === 30)  {
             var g = 'Правильно!';
             document.getElementById("ab").innerHTML = g;
     } else {
-        var g = 'Неправильно! Ответ: 0.7';
+        var g = 'Неправильно! Ответ: 30';
         document.getElementById("ab").innerHTML = g;
     }
 });
 let c = document.getElementById('aac');
 c.addEventListener('click', () => {
     var ic = Number(document.querySelector('#ic>input').value);
-    if (ic === 2) {
+    if (ic === 25) {
         var g = 'Правильно!';
         document.getElementById("ac").innerHTML = g;
     } else {
-        var g = 'Неправильно! Ответ: 2';
+        var g = 'Неправильно! Ответ: 25';
         document.getElementById("ac").innerHTML = g;
     }
 });
 let d = document.getElementById('aad');
 d.addEventListener('click', () => {
     var id = Number(document.querySelector('#id>input').value);
-    if (id === 160) {
+    if (id === 55) {
         var g = 'Правильно!';
         document.getElementById("ad").innerHTML = g;
     } else {
-        var g = 'Неправильно! Ответ: 33';
+        var g = 'Неправильно! Ответ: 55';
         document.getElementById("ad").innerHTML = g;
     }
 });
